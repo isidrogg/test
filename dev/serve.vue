9b73d054eeb6d719ccd915bcb45e5d34fe0760f7
Isidro García <isidro.garcia.partner@decathlon.com>
2020-06-25 13:29:26 +0200

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import Ejemplo from '@/ejemplo.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Ejemplo
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <ejemplo />
+  </div>
+</template>
